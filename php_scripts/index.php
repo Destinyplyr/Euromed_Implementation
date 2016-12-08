62e744f0fd72e3bd3fff09689893d5303a92cfc7
Vasileios Vrouliotis <thelocodmw2@gmail.com>
2016-12-08 14:45:04 +0200

    Meaningless commit along with folders

---
+++ php_scripts/index.php
@@ -0,0 +1,15 @@
+<?php   //sample index 
+
+echo <<<_END
+    <html>
+        <head>
+            <title> Euromed 2016 | Home </title>
+            <meta charset ="utf-8">
+            //<link rel="stylesheet" type="text/css" href ="labStyle.css">
+        </head>
+        <body>
+            
+        </body>
+    </html>
+    _END;
+?>
