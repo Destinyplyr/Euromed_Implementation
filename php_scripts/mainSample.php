eb12778e7659ec4ca7fc8f77441b0c01837548ae
Vasileios Vrouliotis <thelocodmw2@gmail.com>
2016-12-10 16:35:09 +0200

    Navbar and Intro Sample

---
+++ php_scripts/mainSample.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> Euromed 2016 | Home </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href ="../css_scripts/homeCSS.css">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato">
+    <!-- <link rel="stylesheet" type="text/css" href ="bootstrap.min.css">
+    <script type="text/javascript" src="bootstrap.min.js" ></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+  </head>
+  <body>
+
+
+
+    <ul>
+      <div id="media">
+        <a href="http://www.w3schools.com">
+        <img  alt="facebook" src="../images/_facebookIcon.png" width="50" height="50">
+        </a>
+
+        <a href="http://www.hello.com">
+        <img  alt="twitter" src="../images/_twitterIcon.png" width="50" height="50"> </a>
+
+        <a href="http://www.w3schools.com">
+        <img  alt="instagram" src="../images/_instagramIcon.png" width="50" height="50">
+        </a>
+      </div>
+
+
+      <li><a href="#contact"> Contact </a></li>
+      <li> <a href="#faq"> FAQ </a></li>
+      <li style="padding-left: 25em" class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn"> Visitors </a>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </li>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn"> Speakers </a>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </li>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn"> Exhibitors </a>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </li>
+      <li><a href="#venue"> Venue </a></li>
+      <li><a href="#prev_events"> Previous Events </a></li>
+      <li><a href="#tickets"> TICKETS </a></li>
+    </ul>
+
+    <h1> <strong> EUROMED 2016 &nbsp &nbsp &nbsp &nbsp 31 October - 5 November 2016 </strong> </h1> 
+    <p> Digital Cultural Heritage : Documentation <br>
+    Preservation and Protection </p>
+    <p> Filoxenia Conference Center | Nicosia, Cyprus </p>
+  </body>
+</html>
---
+++ php_scripts/mySampleIndex.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title> Euromed 2016 | Home </title>
+		<meta charset ="utf-8">
+		<link rel="stylesheet" type="text/css" href ="homeCSS.css">
+		</head>
+	<body>
+	<!-- 	<?php   //sample index 
+		echo  "Hello PHP @@@@@@"
+		?> -->
+		<div id="media">
+			<a href="http://www.w3schools.com">
+			<img  alt="W3Schools" src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview.png" width="50" height="50">
+			</a>
+
+			<a href="http://www.hello.com">
+			<img  alt="W3Schools" src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697029-twitter-512.png" width="50" height="50"> </a>
+
+			<a href="http://www.w3schools.com">
+			<img  alt="W3Schools" src="https://www.cmich.edu/ess/oss/PublishingImages/new-instagram-icon-topic.png" width="50" height="50">
+			</a>
+		</div>
+	</body>
+</html>
+
+
+
